e270e40aa6809f85f9f306b5f7e094a16791f2c2
sindhuuu31 <svsindhuja23@gmail.com>
2026-03-05 16:39:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,5 +48,6 @@ def predict():
         return jsonify({'vector': 'SYSTEM_ERROR', 'analyzer': str(e)})
 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
     
